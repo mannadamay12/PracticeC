a77bf82f5c8da2720f9fe99d15cf34f701f23696
unknown <meadamann2002@gmail.com>
2021-05-16 12:09:23 +0530

    Char arrays done

---
+++ Arrays/Character/Palindrome_Check.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    char arr[n+1];
+    cin>>arr;
+
+    bool check = 1;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] != arr[n-1-i]){
+            check = 0;
+            break;
+        }
+    }
+
+    if(check == true){
+        cout<<arr<<" is a palindrome"<<endl;
+    }
+    else{
+        cout<<arr<<" is not a palindrome"<<endl;
+    }
+
+    return 0;
+}
